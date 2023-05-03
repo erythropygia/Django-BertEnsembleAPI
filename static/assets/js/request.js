71baee0ba7c1758b4976f8f4671b66b29da0c7eb
root <root@DESKTOP-HLFA2SC.localdomain>
2023-05-03 14:44:21 +0300

    Frontend bugs fixed

--- static/assets/js/request.js
+++ static/assets/js/request.js
@@ -5,6 +5,12 @@ function processText(auth_token) {
   // disable the button to prevent further processing
   //processButton.disabled = true;
 
+  if(inputText.length == 0){
+    document.getElementById("result").innerHTML = "<strong>   Please enter text in the textfield. </strong>";
+    getVisible(0);
+    return 0;
+  }
+
   var xhr = new XMLHttpRequest();
   xhr.open("GET", "http://localhost:8000/process?text=" + inputText);
   xhr.setRequestHeader("Authorization", "Token " + auth_token);
@@ -17,32 +23,36 @@ function processText(auth_token) {
           var result = JSON.parse(response);
           var entities = result["ner"]; // NER entity group 
           var nerWords = [];
-          var usedEntities = new Set();
+
           entities.forEach(function(entity) {
             var word = entity["word"].replace(" ' " , "'");
             var entity_group = entity["entity_group"];
             var color = getColor(entity_group);
             var nerColor = getColorNer(entity_group);
-            if (!usedEntities.has(entity_group)) {
-                usedEntities.add(entity_group);
-                nerWords.push({"word": word, "color": color, "nercolor": nerColor, "entity":entity_group});
+    
+            nerWords.push({"word": word, "entity": entity_group, "color": color, "nercolor": nerColor});
+          });
+
+          var table = "<table class='ner-table'><tr><th>Word</th><th>Entity</th></tr>";
+          var uniquePairs = {}; // Boş bir nesne oluşturarak, daha önce eklenen kelimeleri ve etiketleri tutacağız.
+          
+          nerWords.forEach(function(nerWord) {
+            var pair = nerWord["word"] + "-" + nerWord["entity"]; // Kelime ve etiket çiftini tek bir metin olarak birleştiriyoruz.
+          
+            if (!uniquePairs[pair]) { // Eğer bu çift daha önce eklenmemişse,
+              uniquePairs[pair] = true; // Kontrol nesnesine bu çifti ekliyoruz.
+              var entityTag = "<span class='ner-tag " + nerWord["entity"] + "' style='" + getColorNer(nerWord["entity"]) + "'>" + nerWord["entity"] + "</span>";
+              table += "<tr><td>" + nerWord["word"] + "</td><td>" + entityTag + "</td></tr>"; // Yeni bir satır ekliyoruz.
             }
-        });
-        
-
-          if(entities.length != 0){
-            nerWords.forEach(function(nerWord) {
-              inputText = inputText.replace(new RegExp("\\b" + nerWord["word"] + "\\b", "g"), "<span style='" + nerWord["color"] + "'>" + nerWord["word"] + "<span style='" + nerWord["nercolor"] + "'>" + nerWord["entity"]+ "</span>" + "</span>");
-            });
-          }else{
-              inputText = "Not Found";
-          }
-
+          });
+          
+          table += "</table>";
+              
           
           document.getElementById("result_sentiment").innerHTML = "<strong>Sentiment:</strong> " + result.sentiment.sentiment + ", <strong>Score:</strong> " + result.sentiment.score;
           document.getElementById("result_subject").innerHTML = "<strong>Subject:</strong> " + result.subject.subject + ", <strong>Score:</strong> " + result.subject.score;
           document.getElementById("result_summarize").innerHTML = "<strong>Summary:</strong> " + result.summarize.summary;
-          document.getElementById("result_ner").innerHTML = "<strong>Entities:</strong> " + inputText;
+          document.getElementById("result_ner").innerHTML = "<strong>Entities:</strong> " + table
           getVisible(1);
         } catch (e) {
           console.log("Error parsing response:", e);
@@ -73,11 +83,11 @@ function getColor(entity_group) {
 
 function getColorNer(entity_group) {
   if (entity_group == "LOC") {
-    return "background-color: #FFC0CB; color: #222; border-radius: 3px; padding: 2px; font-size:13px;";
+    return "background-color: #E5B4B7; color: #222; border-radius: 3px; padding: 2px; font-size:13px;";
   } else if (entity_group == "ORG") {
-    return "background-color: #FFD700; color: #222; border-radius: 3px; padding: 2px; font-size:13px; ";
+    return "background-color: #E5CC8D; color: #222; border-radius: 3px; padding: 2px; font-size:13px; ";
   } else if (entity_group == "PER") {
-    return "background-color: #00CED1; color: #222; border-radius: 3px; padding: 2px; font-size:13px;";
+    return "background-color: #A3CEDB; color: #222; border-radius: 3px; padding: 2px; font-size:13px;";
   } 
 }
 
